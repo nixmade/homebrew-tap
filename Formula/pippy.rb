d14648837a30b1f3aae53839336bd48fcc022b37
Mr Awesome <mrawesomenix@gmail.com>
2024-07-16 21:31:17 -0700

    upgrade pippy to v1.2.0

--- Formula/pippy.rb
+++ Formula/pippy.rb
@@ -1,11 +1,11 @@
 class Pippy < Formula
   desc "Create configurable pipelines on existing github action workflows"
   homepage "https://pippy.dev"
-  version "1.1.1"
+  version "1.2.0"
   
   on_macos do
-    url "https://github.com/nixmade/pippy/releases/download/v1.1.1/pippy_Darwin_all.tar.gz"
-    sha256 "8e4e53387f4cbfe8c2ad7a7698b67aa88f2e9af15683f0e2bf44f0e2a7254f4f"
+    url "https://github.com/nixmade/pippy/releases/download/v1.2.0/pippy_Darwin_all.tar.gz"
+    sha256 "73b902c9f7db3b3ce60c38b2ad026ae0f6b4be58161c8903b254efb41142dc38"
     def install
       bin.install "pippy"
     end
@@ -14,8 +14,8 @@ class Pippy < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/nixmade/pippy/releases/download/v1.1.1/pippy_Linux_x86_64.tar.gz"
-        sha256 "e69fb8e7a3728f00870e3c0d51f4b8dfc89976787a5507eed53d0f8cf481a7fd"
+        url "https://github.com/nixmade/pippy/releases/download/v1.2.0/pippy_Linux_x86_64.tar.gz"
+        sha256 "05a2e4cd299181eb492f9f9f7383b45de63fa00ba6b1b924e6d8e0698e3a3957"
 
         def install
           bin.install "pippy"
@@ -24,8 +24,8 @@ class Pippy < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/nixmade/pippy/releases/download/v1.1.1/pippy_Linux_arm64.tar.gz"
-        sha256 "e612080b0808d40646e940389a045e0121720ccde77da699b1d2ccd498e89188"
+        url "https://github.com/nixmade/pippy/releases/download/v1.2.0/pippy_Linux_arm64.tar.gz"
+        sha256 "1ca1524e8c3f3fb6e9b18f94c270ff90579b815515dee525b3be9742e3e2b975"
 
         def install
           bin.install "pippy"
